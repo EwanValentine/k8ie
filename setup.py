795fcf3162d5d64c5821d9aff037c1387c918a25
Ewan Valentine <ewan.valentine@bbc.co.uk>
2017-10-21 12:23:46 +0100

    FIXED dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='k8tie',
+    name='k8ie',
     version='0.1',
-    py_modules=['k8tie'],
+    py_modules=['k8ie'],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
-        'yaml',
         'Jinja2',
+        'pyyaml',
     ],
     entry_points='''
         [console_scripts]
-        k8tie=k8tie:cli
+        k8ie=k8ie:cli
     ''',
 )
